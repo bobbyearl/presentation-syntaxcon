ae16f7ddd6c70784504f10b55c87e0149e5a7353
Bobby Earl <bobby.earl@blackbaud.com>
2017-05-23 09:36:54 -0400

    Added printing capability

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,10 @@
   const Reveal = require('reveal.js');
   require('reveal.js/css/reveal.css');
   require('reveal.js/css/theme/white.css');
+  
+  if (window.location.search.match(/print-pdf/gi)) {
+    require('reveal.js/css/print/pdf.css');
+  }
 
   Reveal.initialize({
     history: true
